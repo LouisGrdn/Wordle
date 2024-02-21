b493c04fa8e011ed4cd921812fed42353b3c27d2
Louis Gardin <louis.gardin@etudiant.univ-reims.fr>
2024-02-22 00:06:44 +0100

    Adding the keyboard + pressing keys to enter a word in progress

--- App.jsx
+++ App.jsx
@@ -1,22 +1,46 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
 import Word from './components/Word';
+import Keyboard from './components/Keyboard'
 
 export default function App() {
+  const [words, setWords] = useState(() => {
+    const tab = [];
+    for(let i = 0; i < 6; i++){
+      tab.push({id: {i}, value: ''})
+    }
+    return tab;
+  });
+
+  function onPress(id, letter) {
+    setWords(words => {
+      return words.map(word => {
+        if(word.id === id){
+          return {...word, value: word.value+letter};
+        } else {
+          return word;
+        }
+      })
+      })
+  };
+
   return (
     <View style={styles.container}>
-      <Word word='test'/>
-      <Word word='test'/>
-      <Word word='test'/>
-      <Word word='test'/>
-      <Word word='test'/>
+      <View style={{flex: 1}}>
+      {words.map((word, index) => (
+        <Word key={index} word={word.value}/>
+      ))}
+      </View>
+      <View style={{flex: 1}}><Keyboard onClick={onPress}/></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 100,
+    flex: 1,
+    height: '100%',
+    marginTop: 80,
     flexDirection: 'column',
     alignItems: 'center'
   }
