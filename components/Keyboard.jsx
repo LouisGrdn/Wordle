2ec33d483937b0d41dcd1ec5fbcd056cd1787247
Louis Gardin <louis.gardin@etudiant.univ-reims.fr>
2024-02-23 00:32:19 +0100

    Adding delete + validate button

--- components/Keyboard.jsx
+++ components/Keyboard.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import PropTypes from "prop-types";
-import Letter from "./Letter";
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faDeleteLeft, faCheck } from '@fortawesome/free-solid-svg-icons'
 
-export default function Keyboard({onClick = null, index = 0}){
+export default function Keyboard({onChange = null, onValidate = null, words = [], turn = 0}){
     const alphabet = [
         'A', 'B', 'C', 'D',
         'E', 'F', 'G', 'H',
@@ -13,18 +14,53 @@ export default function Keyboard({onClick = null, index = 0}){
         'U', 'V', 'W', 'X',
         'Y', 'Z'];
 
+    function onPress(id, letter) {
+      onChange(words => {
+        return words.map(word => {
+          if(word.id === id){
+            return {...word, value: word.value+letter};
+          } else {
+            return word;
+          }
+        })
+        })
+    };
+
+    function onDelete(id) {
+      onChange(words => {
+        return words.map(word => {
+          if(word.id === id){
+            console.log(word.value.slice(0, word.value.count-1));
+            return {...word, value: word.value.slice(0, -1)};
+          } else {
+            return word;
+          }
+        })
+        })
+    }
+
     return (
         <View style={styles.container}>
-      {alphabet.map((letter, index) => (
-        <Letter key={index} name={letter} onPress={onClick}/>
-      ))}
-    </View>
+          {alphabet.map((letter, index) => (
+            <TouchableOpacity onPress={() => onPress(turn, letter)} style={styles.item}>
+                <Text style={styles.letter}>{letter}</Text>
+            </TouchableOpacity>
+          ))}
+          <TouchableOpacity onPress={() => onDelete(turn)} style={styles.item}>
+              <FontAwesomeIcon style={styles.letter} icon={faDeleteLeft} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => onValidate(turn)} style={styles.item}>
+              <FontAwesomeIcon style={styles.letter} icon={faCheck} />
+          </TouchableOpacity>
+        </View>
     );
 }
 
 Keyboard.propTypes = {
-  onPress: PropTypes.func,
-  index: PropTypes.number,
+  onChange: PropTypes.func,
+  onValidate: PropTypes.func,
+  words: PropTypes.array,
+  turn: PropTypes.number,
 }
 
 const styles = StyleSheet.create({
@@ -36,8 +72,14 @@ const styles = StyleSheet.create({
       alignContent: 'center',
       padding: 10,
     },
-    letter: {
-      fontSize: 20,
-      margin: 5,
+    item: {
+      borderWidth: 1,
+      height: 50,
+      width: 50,
+      borderRadius: 10,
+      justifyContent: 'center',
     },
+    letter: {
+        alignSelf: 'center'
+    }
   });
