893e155d112ef367b6687c29a322cb524f47c27b
Louis Gardin <louis.gardin@etudiant.univ-reims.fr>
2024-02-23 00:32:01 +0100

    Adding the color change when answering

--- components/Word.jsx
+++ components/Word.jsx
@@ -3,13 +3,22 @@ import PropTypes from "prop-types";
 import { View, StyleSheet } from "react-native";
 import GuessCard from "./GuessCard";
 
-export default function Word({word = ''}){
+export default function Word({word = '', validate = false, answer = ""}){
+
+    function getColor(index) {
+        if(!validate) {
+            return 'white';
+        }
+        else {
+            return answer.toUpperCase().includes(word[index].toUpperCase()) ? 'green':'red';
+        }
+    }
 
     const numbers = Array.from({ length: 4 }, (_, index) => index);
     return (
         <View style={styles.container}>
             {numbers.map((char, index) => (
-                <GuessCard key={index} letter={word[index] ? word[index] : ''}/>
+                <GuessCard key={index} letter={word[index] ? word[index] : ''} color={getColor(index)}/>
             ))}
         </View>
     )
@@ -23,4 +32,6 @@ const styles = StyleSheet.create({
 
 Word.propTypes = {
     word: PropTypes.string,
+    validate: PropTypes.bool,
+    answer: PropTypes.string,
 }
